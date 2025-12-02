721fccb58e1853576714a5f5ccd340a7aaaff2c7
Antoine Cotten <hello@acotten.com>
2025-12-02 16:01:38 +0100

    Day 1, part 1

---
+++ src/solutions/day01.rs
@@ -0,0 +1,88 @@
+use crate::util::parse::ParseOps;
+use Direction::{Left, Right};
+
+pub enum Direction {
+    Left(u32),
+    Right(u32),
+}
+
+impl Direction {
+    pub fn distance(&self) -> u32 {
+        match *self {
+            Left(dist) | Right(dist) => dist,
+        }
+    }
+}
+
+pub fn parse(input: &str) -> Vec<Direction> {
+    input
+        .lines()
+        .map(|l| {
+            let dist = l.unsigned();
+            match l.chars().take(1).next() {
+                Some('L') => Left(dist),
+                _ => Right(dist),
+            }
+        })
+        .collect()
+}
+
+const MAX_POS: u32 = 100;
+const INIT_POS: u32 = 50;
+
+pub fn part1(rotations: &[Direction]) -> u32 {
+    rotations
+        .iter()
+        .scan(INIT_POS, |pos, dir| {
+            *pos = match dir {
+                Left(dist) => (MAX_POS - ((MAX_POS - *pos) + dist) % MAX_POS) % MAX_POS,
+                Right(dist) => (*pos + dist) % MAX_POS,
+            };
+            Some(*pos == 0 && dir.distance() > 0)
+        })
+        .filter(|rotated_to_zero| *rotated_to_zero)
+        .count() as u32
+}
+
+pub fn part2(_: &[Direction]) -> u32 {
+    0
+}
+
+#[test]
+fn sample_input() {
+    let input = "\
+        L68\n\
+        L30\n\
+        R48\n\
+        L5\n\
+        R60\n\
+        L55\n\
+        L1\n\
+        L99\n\
+        R14\n\
+        L82\
+        ";
+    let rotations = parse(input);
+    assert_eq!(part1(&rotations), 3);
+}
+
+#[test]
+fn overflow() {
+    let input = "\
+        L200\n\
+        R50\
+        ";
+    let passw = parse(input);
+    assert_eq!(part1(&passw), 1);
+}
+
+#[test]
+fn no_recount_zero_dist() {
+    let input = "\
+        L50\n\
+        L0\n\
+        R0\
+        ";
+    let passw = parse(input);
+    assert_eq!(part1(&passw), 1);
+}
