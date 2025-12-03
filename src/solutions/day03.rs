650380a97657d350e9b68b4e6bc74712a78a3a24
Antoine Cotten <hello@acotten.com>
2025-12-03 21:49:30 +0100

    Day 3, part 2

--- src/solutions/day03.rs
+++ src/solutions/day03.rs
@@ -26,8 +26,31 @@ pub fn part1(banks: &[Vec<u8>]) -> u64 {
         .sum()
 }
 
-pub fn part2(_: &[Vec<u8>]) -> u64 {
-    0
+pub fn part2(banks: &[Vec<u8>]) -> u64 {
+    const JOLT_DIGITS: usize = 12;
+
+    banks
+        .iter()
+        .map(|b| {
+            (1..=JOLT_DIGITS)
+                .scan((0, 0), |(j_max, j_max_pos), step| {
+                    *j_max = 0;
+                    let skip = if step == 1 { 0 } else { *j_max_pos + 1 };
+                    b.iter()
+                        .enumerate()
+                        .skip(skip)
+                        .take(b.len() - skip - (JOLT_DIGITS - step))
+                        .for_each(|(i, j)| {
+                            if j > j_max {
+                                *j_max = *j;
+                                *j_max_pos = i;
+                            }
+                        });
+                    Some((*j_max as u64) * 10u64.pow((JOLT_DIGITS - step) as u32))
+                })
+                .sum::<u64>()
+        })
+        .sum()
 }
 
 #[test]
@@ -40,14 +63,19 @@ fn sample_input() {
         ";
     let jolts = parse(input);
     assert_eq!(part1(&jolts), 98 + 89 + 78 + 92);
+    assert_eq!(
+        part2(&jolts),
+        987654321111 + 811111111119 + 434234234278 + 888911112111
+    );
 }
 
 #[test]
 fn max_at_boundaries() {
     let input = "\
-        900000000000000\n\
-        000000000000009\
+        911111111111111\n\
+        111111111111119\
         ";
     let banks = parse(input);
-    assert_eq!(part1(&banks), 90 + 9);
+    assert_eq!(part1(&banks), 91 + 19);
+    assert_eq!(part2(&banks), 911111111111 + 111111111119);
 }
